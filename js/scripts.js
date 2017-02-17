57f4142a05789eb920456875ea6e9e995ac8f097
jahan <jwalsh370@gmail.com>
2017-02-17 11:34:00 -0800

    add function to js, use debugger to find bugs in js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,39 @@
 $(function(){
 
-  $(".ownInfo").submit(function(){
+  $(".ownInfo").submit(function() {
+    event.preventDefault();
+
     var name1 = $("input#firstName").val();
     var name2 = $("input#lastName").val();
 
     $(".firstName").text(name1);
     $(".lastName").text(name2);
+debugger;
+    // function add(option1, option2, option3, option5)
+    //       option1 + option2 + option3 + option5;
 
-    var learn = $("input:radio[name=learn]:checked").val();
-    var practice = $("#practice").val();
-    var heavy = $("#heavy").val();
-    var solve = $("#solve").val();
-    var front = $("#front").val();
+    // var result = add(option1, option2, option3, option5).val();
+
+    // var learn = $("input:radio[name=learn]:checked").val();
+    var option1 = parseInt($(".option1").val());
+    var option2 = parseInt($(".option2").val());
+    var option3 = parseInt($(".option3").val());
+    var option5 = parseInt($(".option5").val());
+
+
+
+    if (result > 6){
+      course = ("Css");
+    } else if (result = 3){
+      course = ("PHP");
+    } else if (result = 5) {
+      course = ("java")
+    } else if (result < 5 && result > 1){
+      course = ("ruby")
+    } else (result = 0)
+      course = ("something else!")
+    });
+
+    $("#course").text(result);
 
   });
-});
