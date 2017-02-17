ed8cc2270decabf957bcdd32f3a33512ad8d95d7
jahan <jwalsh370@gmail.com>
2017-02-17 11:54:58 -0800

    add functional javascript functions, fix grammatical errors and spacing issues in html text.

--- js/scripts.js
+++ js/scripts.js
@@ -8,32 +8,18 @@ $(function(){
 
     $(".firstName").text(name1);
     $(".lastName").text(name2);
-debugger;
-    // function add(option1, option2, option3, option5)
-    //       option1 + option2 + option3 + option5;
 
-    // var result = add(option1, option2, option3, option5).val();
+    var learn = $("input:radio[name=learn]:checked").val();
 
-    // var learn = $("input:radio[name=learn]:checked").val();
-    var option1 = parseInt($(".option1").val());
-    var option2 = parseInt($(".option2").val());
-    var option3 = parseInt($(".option3").val());
-    var option5 = parseInt($(".option5").val());
-
-
-
-    if (result > 6){
-      course = ("Css");
-    } else if (result = 3){
-      course = ("PHP");
-    } else if (result = 5) {
-      course = ("java")
-    } else if (result < 5 && result > 1){
-      course = ("ruby")
-    } else (result = 0)
-      course = ("something else!")
-    });
-
-    $("#course").text(result);
+        if (learn === "ready"){
+          course = 'Ruby, Php, and Java.'
+        } else if ( learn === "no") {
+          course = "Css, Design, and Java."
+        } else {
+          course = "something else."
+        }
+    $("#course").empty().append(course);
 
   });
+
+});
